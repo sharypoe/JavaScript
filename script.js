e2d489cf240c3284a58b53cdbd37c99addfe6f45
Shary Batista <sharypoe@gmail.com>
2022-08-24 09:48:11 -0400

    Added comments to JavaScript file.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function: creates a new paragraph and appends it to the bottom of the HTML body.
 function createParagraph() 
 {
   const para = document.createElement('p');
@@ -5,9 +6,10 @@ function createParagraph()
   document.body.appendChild(para);
 }
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button'); // Get references to all the buttons on the page in an array format.
 
-for (const button of buttons) 
+for (const button of buttons) //  Loop through all the buttons 
 {
-  button.addEventListener('click', createParagraph);
+  button.addEventListener('click', createParagraph); // Add a click event listener to each one.
+  //   When any button is pressed, the createParagraph() function will be run.
 }
